11487f7cd36048d23443c6d6bdd7147bbb120413
Cedric Kiplimo <kiplimock@gmail.com>
2023-02-20 21:56:01 +0300

    js code to add todo items

---
+++ app.js
@@ -0,0 +1,39 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo)
+
+// Functions
+function addTodo(event) {
+    // prevent form from submitting
+    event.preventDefault();
+
+    // todo div and add class
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    // create LI and append to div
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'Hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo)
+    
+    // CHECK MARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    // DELETE BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    // APPEND TO LIST
+    todoList.appendChild(todoDiv)
+}
